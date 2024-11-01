921c2e99ae4547898f45a066cec24493d4bf57a4
Andrew Huang <ahuang@sdccd.edu>
2024-11-01 09:01:01 -0700

    migrated to JavaFX sample code, still need to break up the code in to objects

---
+++ src/module-info.java
@@ -0,0 +1,11 @@
+module co.ppg2 {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+    requires javafx.graphics;
+    requires javafx.media;
+    requires jdk.jsobject;
+    requires java.desktop;
+
+    exports co.ppg2;
+}
